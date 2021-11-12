40f979f6af177dc31847f109a9a4eacf6682e3fd
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-12 09:23:03 +0530

    framework added

---
+++ framework1/framework1/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  framework1
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+import Foundation
+public class TestClass{
+    public class func testOne(){
+        
+    }
+}
---
+++ framework1/framework1/framework1.h
@@ -0,0 +1,18 @@
+//
+//  framework1.h
+//  framework1
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework1.
+FOUNDATION_EXPORT double framework1VersionNumber;
+
+//! Project version string for framework1.
+FOUNDATION_EXPORT const unsigned char framework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework1/PublicHeader.h>
+
+
